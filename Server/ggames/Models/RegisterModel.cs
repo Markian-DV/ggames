0c219cf7e0507351b26e6fa992c27c10a9a527c4
Markian-DV <gvintua@gmail.com>
2021-04-04 16:36:20 +0300

    created WebApi Proj, Added Simple authentication
    added authentication using identity + JWT. Created Models and Services
    for authentication. Basic JWT settings added
    the connection to the local db (sql server) is set up. added Migrations.
    Allowed cross-domain requests

---
+++ Server/ggames/Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ggames.Models
+{
+    public class RegisterModel
+    {   
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Server/ggames/Services/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using ggames.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ggames.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthResult> RegisterAsync(string email, string password, string username);
+
+        Task<AuthResult> LoginAsync(string email, string password);
+    }
+}
