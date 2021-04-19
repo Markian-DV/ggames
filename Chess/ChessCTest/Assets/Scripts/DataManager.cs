51b4a7a533364e60866718fc529c90ee339d743d
Rev1kon <Andrii.Klochkovskyi@gmail.com>
2021-04-19 12:50:15 +0300

    Chess with Loading scene + JSHook and ExternalCall build for Unity -> front-end calls

---
+++ Chess/ChessCTest/Assets/Scripts/DataManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static string nickname = "";
+    public static bool isPlayerWhite = false;
+    public static bool isPlayerBlack = false;
+    public static int Score = 800;
+}
---
+++ Chess/ChessCTest/Assets/Scripts/JSManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JSManager : MonoBehaviour
+{
+    public void GetNickname(string nickname) 
+    {
+        DataManager.nickname = nickname;
+        SceneManager.LoadScene(1);
+    }
+}
