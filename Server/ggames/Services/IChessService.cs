370ae860c3babe657791264200647bec533352e4
Markian-DV <gvintua@gmail.com>
2021-04-21 17:27:18 +0300

    added chess rating service
    added Chess rating model
    added IChessService with get all ratings, update and get rating
    by user id. Implemented it in ChessService

---
+++ Server/ggames/Services/IChessService.cs
@@ -0,0 +1,19 @@
+﻿using ggames.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ggames.Services
+{
+    public interface IChessService
+    {
+        //get all ratings
+        Task<List<ChessRating>> GetRatingAsync();
+        //get rating by id
+        Task<ChessRating> GetRatingByUserIdAsync(Guid UserId);
+
+        //add rating
+        Task<bool> UpdateRatingAsync(Guid UserId, int rating);
+    }
+}
